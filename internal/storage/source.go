ed87922b3e426fe5f885484699b06037a6af16cf
w212w <edaev.bilal@icloud.com>
2024-12-22 07:15:37 +0300

    source.go added func

--- internal/storage/source.go
+++ internal/storage/source.go
@@ -1,9 +1,36 @@
 package storage
 
 import (
+	"TelegramBotGolang/internal/model"
+	"context"
+	"time"
+
 	"github.com/jmoiron/sqlx"
+	"github.com/samber/lo"
 )
 
 type SourcePostgresStorage struct {
 	db *sqlx.DB
+}
+
+type dbSource struct {
+	ID        int64     `db:"id"`
+	Name      string    `db:"name"`
+	FeedUrl   string    `db:"feed_url"`
+	CreatedAt time.Time `db:"created_at"`
+}
+
+func (s *SourcePostgresStorage) Sources(ctx context.Context) ([]model.Source, error) {
+	conn, err := s.db.Connx(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	var sources []dbSource
+	if err := conn.SelectContext(ctx, &sources, `SELECT * FROM sources`); err != nil {
+		return nil, err
+	}
+
+	return lo.Map(sources, func(source dbSource, _ int) model.Source { return model.Source(source) }), nil
 }
