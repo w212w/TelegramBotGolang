fe3454b6288ee83166cab5c21e4514af21ecae3b
w212w <edaev.bilal@icloud.com>
2024-12-23 00:56:49 +0300

    internal/storage.go completed

--- internal/storage/source.go
+++ internal/storage/source.go
@@ -34,3 +34,59 @@ func (s *SourcePostgresStorage) Sources(ctx context.Context) ([]model.Source, er
 
 	return lo.Map(sources, func(source dbSource, _ int) model.Source { return model.Source(source) }), nil
 }
+
+func (s *SourcePostgresStorage) SourceByID(ctx context.Context, id int64) (*model.Source, error) {
+	conn, err := s.db.Connx(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	var source dbSource
+	if err := conn.GetContext(ctx, &source, `SELECT * FROM sources WHERE id = $1`, id); err != nil {
+		return nil, err
+	}
+
+	return (*model.Source)(&source), nil
+}
+
+func (s *SourcePostgresStorage) Add(ctx context.Context, source model.Source) (int64, error) {
+	conn, err := s.db.Connx(ctx)
+	if err != nil {
+		return 0, err
+	}
+	defer conn.Close()
+
+	var id int64
+	row := conn.QueryRowxContext(
+		ctx,
+		query: `INSERT INTO sources (name, feed_url, created_at) VALUES ($1, $2, $3) RETURNING id`,
+		source.Name,
+		source.FeedUrl
+		source.CreatedAt,
+	)
+	
+	if err := row.Err(); err != nil {
+		return 0, err
+	}
+
+	if err := row.Scan(&id); err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
+func (s *SourcePostgresStorage) Delete (ctx context.Context, id int64) error {
+	conn, err := s.db.Connx(ctx)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	if _, err := conn.ExecContext(ctx, `DELETE FROM source WHERE id = $1`, id); err != nil {
+		return err
+	}
+
+	return nil
+}
