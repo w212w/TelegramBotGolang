e12ed85b7b4ace30487c7168e049d635ec2ab19d
w212w <edaev.bilal@icloud.com>
2024-12-23 11:53:29 +0300

    storage/article.go completed

--- internal/storage/migrations/20241220022248_create_articles.sql
+++ internal/storage/migrations/20241220022248_create_articles.sql
@@ -4,15 +4,15 @@ CREATE TABLE articles (
     id SERIAL PRIMARY KEY,
     source_id INT NOT NULL,
     title VARCHAR(255) NOT NULL,
-    link VARCHAR(255) NOT NULL,
+    link VARCHAR(255) NOT NULL UNIQUE,
     summary TEXT NOT NULL,
     published_at TIMESTAMP NOT NULL,
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP
 );
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE IF EXISTS sources;
+DROP TABLE IF EXISTS articles;
 -- +goose StatementEnd
